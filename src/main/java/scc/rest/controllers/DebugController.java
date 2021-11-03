dfbae1cf2a3f16663695e8569ec858892138ad45
André Palma <andremarquespalma@gmail.com>
2021-11-03 21:08:18 +0000

    Add Environment Variables Endpoint

--- src/main/java/scc/rest/controllers/DebugController.java
+++ src/main/java/scc/rest/controllers/DebugController.java
@@ -1,12 +1,24 @@
 package scc.rest.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @RestController
 @RequestMapping("/ctrl")
 public class DebugController {
+
+    private final Environment environment;
+
+    @Autowired
+    public DebugController(Environment environment) {
+        this.environment = environment;
+    }
 
     /**
      * This method just prints a string. It may be useful to check if the current
@@ -15,6 +27,19 @@ public class DebugController {
     @GetMapping("/version")
     public String hello() {
         return "v: 0001";
+    }
+
+    @GetMapping("/env")
+    public String env() {
+        return Stream.of(
+                        "azure.cosmos.database",
+                        "azure.cosmos.key",
+                        "azure.cosmos.uri",
+                        "azure.storage.account-name",
+                        "azure.storage.account-key"
+                )
+                .map(property -> property + "=" + environment.getProperty(property))
+                .collect(Collectors.joining("\n"));
     }
 
 }
